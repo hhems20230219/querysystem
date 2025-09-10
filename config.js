346d7521ea2d7d2909fb4435a6dd522fc4c35bd6
hhems20230219 <hhems20230219@gmail.com>
2025-09-11 00:07:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // 把這個改成你部署的 Apps Script Web App URL（/exec 結尾）
-const API_BASE = "https://script.google.com/macros/s/你的-WebApp-ID/exec";
+const API_BASE = "https://script.google.com/macros/s/AKfycbxnqS_ron1nCZr1HEryLPEMSpi0YzUs_3IUgHzKRPNHdGe4STG35rm6iFvH5_pzEBsvVQ/exec";
